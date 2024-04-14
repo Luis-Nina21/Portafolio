f473b4248a63438d60b6407dff9e795343406416
soy Luis <luisantonyninallicahua@gmail.com>
2024-04-14 16:20:59 -0500

    actucalizando cambios en todo el proyecto

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', function() {
+    var menu = document.querySelector('.menu');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 0) {
+        menu.classList.add('fixed-menu');
+    } else {
+        menu.classList.remove('fixed-menu');
+    }
+});
+
+
+
+  
